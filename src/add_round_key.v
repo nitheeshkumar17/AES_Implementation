780f0fc66ad10fd272406cc42d74cb8737a4a6ac
Nitheesh Kumar Kethu <nitheesh.nkr@yahoo.com>
2020-03-04 06:27:08 +0530

    Update add_round_key.v

--- src/add_round_key.v
+++ src/add_round_key.v
@@ -1,4 +1,6 @@
-// Tbis module adds the expanded keys to every round of the algorithm
+// This module adds the expanded keys to every round of the algorithm
+// Basically an Addition in Galois Field i.e., XOR Operation
+
 module add_round_key (
 		input [127:0] text,
 		input [127:0] key,
